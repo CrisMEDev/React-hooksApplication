93b00ee175c8c682c9cb3681e161941136d1b6af
Cristian Morales <cr15moresp@gmail.com>
2021-09-20 17:55:05 -0500

    Revision del uso del cleanUp para evitar fugas de memoria con el useEffect

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,22 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const Message = () => {
 
+    const [coords, setCoords] = useState({x:0, y:0});
+
+    const { x, y } = coords;
+
     useEffect(() => {
         
-        // Muestra mensaje la primera vez que se renderiza el componente
-        console.log('componente montado');
+        const mouseMove = (event) => {
+            const coors = { x: event.x, y: event.y };
+            console.log(coors);
+
+            setCoords({ x: coors.x, y: coors.y });
+
+            console.log(' :D ');
+        }
+        
+        window.addEventListener( 'mousemove', mouseMove );
         
         return () => {
-            // Cuando ya no se necesita el componente con el useEffect, se hace una limpieza
-            // con esta funcion de retorno (cleanUp)
-            console.log('componente desmontado');
+            
+            // Se elimina la refencia de mouseMove del eventListener mousemove
+            // para evitar un consumo de memoria innecesario
+            window.removeEventListener( 'mousemove', mouseMove );
+
         }
     }, []);
 
     return (
         <>
             <h3>Eres genial</h3>
+            <h4> x:{x}, y:{y}</h4>
         </>
     );
 }
