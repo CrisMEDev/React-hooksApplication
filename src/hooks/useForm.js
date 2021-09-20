07786bd766a5a82ff2212494971ac409c1a8e129
Cristian Morales <cr15moresp@gmail.com>
2021-09-20 18:26:21 -0500

    Custom hook para el envio de formularios creado

---
+++ src/hooks/useForm.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+export const useForm = ( initialState = {} ) => {
+    
+    const [formValuesState, setFormValuesState] = useState(initialState)
+
+    const handleInputChange = ( { target } ) => {   // Se extrae el target del event
+
+        // console.log(event.target.name);
+        // console.log(event.target.value);
+        setFormValuesState({
+            ...formValuesState,
+            [ target.name ]: target.value   // Se establece el name del input con el valor que se escribe
+                                            // en el input requerido
+        });
+
+    }
+
+    return [ formValuesState, handleInputChange ];
+
+}
