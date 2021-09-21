2a7433a2332b76ebad0820bdc1db1242391a1b87
Cristian Morales <cr15moresp@gmail.com>
2021-09-21 14:58:13 -0500

    revision de los 2 casos de uso del useCallback hook

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import { Memorize } from './components/06-memos/Memorize';
-import { MemoHook } from './components/06-memos/MemoHook';
+// import { MemoHook } from './components/06-memos/MemoHook';
+import { CallbackHook } from './components/06-memos/CallbackHook';
 
 ReactDOM.render(
 
-    <MemoHook />,
+    <CallbackHook />,
 
     document.getElementById('root')
 
