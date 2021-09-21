7ef5793557ed2aba6f32301f5fe3ff871be3e755
Cristian Morales <cr15moresp@gmail.com>
2021-09-21 14:25:38 -0500

    useMemo usado para memorizar un valor y evitar que una funcion se ejecute en el renderizado si el valor no cambia

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Memorize } from './components/06-memos/Memorize';
+// import { Memorize } from './components/06-memos/Memorize';
+import { MemoHook } from './components/06-memos/MemoHook';
 
 ReactDOM.render(
 
-    <Memorize />,
+    <MemoHook />,
 
     document.getElementById('root')
 
