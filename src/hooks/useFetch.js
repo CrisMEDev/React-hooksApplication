c2ee6bd74ad41be37ebc7b6193ff67425864dfdf
Cristian Morales <cr15moresp@gmail.com>
2021-09-21 01:45:54 -0500

    useRef usado para prevenir el error de un mal renderizado

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export const useFetch = ( url ) => {
     
+    const isMounted = useRef(true);
+    
     const [state, setState] = useState({data: null, loading: true, error: null});
+
+    useEffect(() => {
+        return () => {  // Accion a disparar cuando el componente se desmonte
+            isMounted.current = false;
+        }
+    }, []);
 
     useEffect( () => {
 
@@ -13,11 +21,21 @@ export const useFetch = ( url ) => {
             .then( resp => resp.json() )
             .then( data => {
 
-                setState({
-                    loading: false,
-                    error: null,
-                    data
-                });
+                // Se coloca un timeout para intencionalmente provocar un error
+                // en el renderizado al usar el boton show/hide
+                setTimeout( () => {
+
+                    if ( isMounted.current ){
+                        setState({
+                            loading: false,
+                            error: null,
+                            data
+                        });
+                    } else {
+                        console.log('setState no se llamó para el renderizado');
+                    }
+
+                }, 3000);
 
             });
 
